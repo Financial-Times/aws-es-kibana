d8b8e96ae397d19e8c6ee872398827cfc9f8a7a7
ola.okejimi <ola.okejimi@ft.com>
2020-08-25 11:37:12 +0100

    migrate to okta

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
 #!/usr/bin/env node
 
-var AWS = require('aws-sdk');
-var http = require('http');
-var httpProxy = require('http-proxy');
-var express = require('express');
-var bodyParser = require('body-parser');
-var stream = require('stream');
-var figlet = require('figlet');
-var compress = require('compression');
+var AWS = require("aws-sdk");
+var http = require("http");
+var httpProxy = require("http-proxy");
+var express = require("express");
+var bodyParser = require("body-parser");
+var stream = require("stream");
+var figlet = require("figlet");
+var compress = require("compression");
 
-const authS3O = require('@financial-times/s3o-middleware');
+const EdTechAuth = require("@financial-times/ed-tech-auth");
 
-var yargs = require('yargs')
-    .usage('usage: $0 [options] <aws-es-cluster-endpoint>')
-    .option('b', {
-        alias: 'bind-address',
-        default: process.env.BIND_ADDRESS || '127.0.0.1',
-        demand: false,
-        describe: 'the ip address to bind to',
-        type: 'string'
-    })
-    .option('p', {
-        alias: 'port',
-        default: process.env.PORT || 9200,
-        demand: false,
-        describe: 'the port to bind to',
-        type: 'number'
-    })
-    .option('r', {
-        alias: 'region',
-        default: process.env.REGION,
-        demand: false,
-        describe: 'the region of the Elasticsearch cluster',
-        type: 'string'
-    })
-    .option('s', {
-      alias: 'silent',
-      default: false,
-      demand: false,
-      describe: 'remove figlet banner'
-    })
-    .option('l', {
-      alias: 'limit',
-      default: process.env.LIMIT || '1kb',  
-      demand: false,
-      describe: 'request limit'
-    })
-    .help()
-    .version()
-    .strict();
+var yargs = require("yargs")
+  .usage("usage: $0 [options] <aws-es-cluster-endpoint>")
+  .option("b", {
+    alias: "bind-address",
+    default: process.env.BIND_ADDRESS || "127.0.0.1",
+    demand: false,
+    describe: "the ip address to bind to",
+    type: "string",
+  })
+  .option("p", {
+    alias: "port",
+    default: process.env.PORT || 9200,
+    demand: false,
+    describe: "the port to bind to",
+    type: "number",
+  })
+  .option("r", {
+    alias: "region",
+    default: process.env.REGION,
+    demand: false,
+    describe: "the region of the Elasticsearch cluster",
+    type: "string",
+  })
+  .option("s", {
+    alias: "silent",
+    default: false,
+    demand: false,
+    describe: "remove figlet banner",
+  })
+  .option("l", {
+    alias: "limit",
+    default: process.env.LIMIT || "1kb",
+    demand: false,
+    describe: "request limit",
+  })
+  .help()
+  .version()
+  .strict();
 var argv = yargs.argv;
 
 var ENDPOINT = process.env.ENDPOINT || argv._[0];
 
 if (!ENDPOINT) {
-    yargs.showHelp();
-    process.exit(1);
+  yargs.showHelp();
+  process.exit(1);
 }
 
 // Try to infer the region if it is not provided as an argument.
 var REGION = argv.r;
 if (!REGION) {
-    var m = ENDPOINT.match(/\.([^.]+)\.es\.amazonaws\.com\.?$/);
-    if (m) {
-        REGION = m[1];
-    } else {
-        console.error('region cannot be parsed from endpoint address, either the endpoint must end ' +
-                      'in .<region>.es.amazonaws.com or --region should be provided as an argument');
-        yargs.showHelp();
-        process.exit(1);
-    }
+  var m = ENDPOINT.match(/\.([^.]+)\.es\.amazonaws\.com\.?$/);
+  if (m) {
+    REGION = m[1];
+  } else {
+    console.error(
+      "region cannot be parsed from endpoint address, either the endpoint must end " +
+        "in .<region>.es.amazonaws.com or --region should be provided as an argument"
+    );
+    yargs.showHelp();
+    process.exit(1);
+  }
 }
 
 var TARGET = process.env.ENDPOINT || argv._[0];
 if (!TARGET.match(/^https?:\/\//)) {
-    TARGET = 'https://' + TARGET;
+  TARGET = "https://" + TARGET;
 }
 
 var BIND_ADDRESS = argv.b;
@@ -84,78 +86,91 @@ var REQ_LIMIT = argv.l;
 var credentials;
 var chain = new AWS.CredentialProviderChain();
 chain.resolve(function (err, resolved) {
-    if (err) throw err;
-    else credentials = resolved;
+  if (err) throw err;
+  else credentials = resolved;
 });
 
 function getCredentials(req, res, next) {
-    return credentials.get(function (err) {
-        if (err) return next(err);
-        else return next();
-    });
+  return credentials.get(function (err) {
+    if (err) return next(err);
+    else return next();
+  });
 }
 var proxy = httpProxy.createProxyServer({
-    target: TARGET,
-    changeOrigin: true,
-    secure: true
+  target: TARGET,
+  changeOrigin: true,
+  secure: true,
 });
 
 var app = express();
 
-app.enable('trust proxy');
-app.set('s3o-cookie-ttl', 86400000);
-
+app.enable("trust proxy");
 app.use(compress());
-app.use((req, res, next) => {
-    authS3O(req, res, next)
-});
 
-app.use(bodyParser.raw({limit: REQ_LIMIT, type: function() { return true; }}));
+const auth = new EdTechAuth(app);
+app.use(auth.middleware);
+
+app.use(
+  bodyParser.raw({
+    limit: REQ_LIMIT,
+    type: function () {
+      return true;
+    },
+  })
+);
 app.use(getCredentials);
 app.use(function (req, res) {
-    var bufferStream;
-    if (Buffer.isBuffer(req.body)) {
-        var bufferStream = new stream.PassThrough();
-        bufferStream.end(req.body);
-    }
-    proxy.web(req, res, {buffer: bufferStream});
+  var bufferStream;
+  if (Buffer.isBuffer(req.body)) {
+    var bufferStream = new stream.PassThrough();
+    bufferStream.end(req.body);
+  }
+  proxy.web(req, res, { buffer: bufferStream });
 });
 
-proxy.on('proxyReq', function (proxyReq, req) {
-    var endpoint = new AWS.Endpoint(ENDPOINT);
-    var request = new AWS.HttpRequest(endpoint);
-    request.method = proxyReq.method;
-    request.path = proxyReq.path;
-    request.region = REGION;
-    if (Buffer.isBuffer(req.body)) request.body = req.body;
-    if (!request.headers) request.headers = {};
-    request.headers['presigned-expires'] = false;
-    request.headers['Host'] = ENDPOINT;
+proxy.on("proxyReq", function (proxyReq, req) {
+  var endpoint = new AWS.Endpoint(ENDPOINT);
+  var request = new AWS.HttpRequest(endpoint);
+  request.method = proxyReq.method;
+  request.path = proxyReq.path;
+  request.region = REGION;
+  if (Buffer.isBuffer(req.body)) request.body = req.body;
+  if (!request.headers) request.headers = {};
+  request.headers["presigned-expires"] = false;
+  request.headers["Host"] = ENDPOINT;
 
-    var signer = new AWS.Signers.V4(request, 'es');
-    signer.addAuthorization(credentials, new Date());
+  var signer = new AWS.Signers.V4(request, "es");
+  signer.addAuthorization(credentials, new Date());
 
-    proxyReq.setHeader('Host', request.headers['Host']);
-    proxyReq.setHeader('X-Amz-Date', request.headers['X-Amz-Date']);
-    proxyReq.setHeader('Authorization', request.headers['Authorization']);
-    if (request.headers['x-amz-security-token']) proxyReq.setHeader('x-amz-security-token', request.headers['x-amz-security-token']);
+  proxyReq.setHeader("Host", request.headers["Host"]);
+  proxyReq.setHeader("X-Amz-Date", request.headers["X-Amz-Date"]);
+  proxyReq.setHeader("Authorization", request.headers["Authorization"]);
+  if (request.headers["x-amz-security-token"])
+    proxyReq.setHeader(
+      "x-amz-security-token",
+      request.headers["x-amz-security-token"]
+    );
 });
 
-proxy.on('proxyRes', function (proxyReq, req, res) {
-    if (req.url.match(/\.(css|js|img|font)/)) {
-        res.setHeader('Cache-Control', 'public, max-age=86400');
-    }
+proxy.on("proxyRes", function (proxyReq, req, res) {
+  if (req.url.match(/\.(css|js|img|font)/)) {
+    res.setHeader("Cache-Control", "public, max-age=86400");
+  }
 });
 
 http.createServer(app).listen(PORT);
 
-if(!argv.s) {
-    console.log(figlet.textSync('AWS ES Proxy!', {
-        font: 'Speed',
-        horizontalLayout: 'default',
-        verticalLayout: 'default'
-    }));
+if (!argv.s) {
+  console.log(
+    figlet.textSync("AWS ES Proxy!", {
+      font: "Speed",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+    })
+  );
 }
 
-console.log('AWS ES cluster available at http://' + BIND_ADDRESS + ':' + PORT);
-console.log('Kibana available at http://' + BIND_ADDRESS + ':' + PORT + '/_plugin/kibana/');
+console.log("AWS ES cluster available at http://" + BIND_ADDRESS + ":" + PORT);
+console.log(
+  "Kibana available at http://" + BIND_ADDRESS + ":" + PORT + "/_plugin/kibana/"
+);
